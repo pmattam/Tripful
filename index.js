3406b2805e0e0f6b12fb511c79d4e1fad015dc22
Prathyusha Mattam <prathyusha.mattam@gmail.com>
2018-05-09 18:13:16 -0400

    Adding Redux Store

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-AppRegistry.registerComponent('tripful', () => App);
+import React from "react";
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./src/reducers/index";
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+window.store = store;
+
+let reactNativeReduxStore = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+  
+AppRegistry.registerComponent('tripful', () => reactNativeReduxStore);
